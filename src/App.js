98ea6809eac03ff5382a4c0480893db1568a17e8
Casey The Great <casey@caseypitman.com>
2020-09-10 12:01:50 -0500

    JSX mockup created

--- src/App.js
+++ src/App.js
@@ -3,27 +3,33 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      <table className = 'gameBoard' cellSpacing = '0'>
+      <h1 className = 'winner'>X WINS!</h1>
+      <table className = 'game-board' cellSpacing = '0'>
         <tr className = 'title' >
           <th className = 'title-text' colSpan = '3'>Tic Tac Toe</th>
         </tr>
         <tr className = 'row-0'>
-          <td className = 'cell 0-0'>X</td>
-          <td className = 'cell 0-1'>O</td>
-          <td className = 'cell 0-2'>X</td>
+          <td className = 'cell 0-0 win'>X</td>
+          <td className = 'cell 0-1 occupied'>O</td>
+          <td className = 'cell 0-2 empty'></td>
         </tr>
         <tr className = 'row-1'>
-          <td className = 'cell 1-0'>X</td>
-          <td className = 'cell 1-1'>O</td>
-          <td className = 'cell 1-2'>X</td>
+          <td className = 'cell 1-0 occupied'>X</td>
+          <td className = 'cell 1-1 win'>X</td>
+          <td className = 'cell 1-2 occupied'>O</td>
         </tr>
         <tr className = 'row-2'>
-          <td className = 'cell 2-0'>X</td>
-          <td className = 'cell 2-1'>O</td>
-          <td className = 'cell 2-2'>X</td>
+          <td className = 'cell 2-0 occupied'>O</td>
+          <td className = 'cell 2-1 occupied'>O</td>
+          <td className = 'cell 2-2 win'>X</td>
+        </tr> 
+        <tr className = 'footer' >
+          <th className = 'footer-text' colSpan = '3'>Begin Playing</th>
         </tr>
-        
       </table>
+      <div className = 'button-div'>
+        <button className = 'reset'>Play Again</button>
+      </div>
     </div>
   );
 }
