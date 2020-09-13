abdbc6ce4ff51d9690cbdc26c11fa12706a3c198
Casey The Great <casey@caseypitman.com>
2020-09-13 17:53:24 -0500

    Updates

---
+++ precache-manifest.69896bac13834055ad42cd1e67315b0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fbad3e6c43578805b93b129e5b048e1",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "39b8edacdcb23e68500f",
+    "url": "/tic-tac-toe/static/css/main.c4d8596e.chunk.css"
+  },
+  {
+    "revision": "455fda5880e1023c44da",
+    "url": "/tic-tac-toe/static/js/2.3b437646.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.3b437646.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39b8edacdcb23e68500f",
+    "url": "/tic-tac-toe/static/js/main.e1c075d0.chunk.js"
+  },
+  {
+    "revision": "7f32745e7b69030faee7",
+    "url": "/tic-tac-toe/static/js/runtime-main.df28867d.js"
+  }
+]);
